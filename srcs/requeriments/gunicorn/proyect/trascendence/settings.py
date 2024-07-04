b71869e1e380c73e739cf693557887bd41d4c973
Victor Näslund Cuesta <victor.naslund@yahoo.com>
2024-07-04 18:16:09 +0200

    configured allowed hosts

--- srcs/requeriments/gunicorn/proyect/trascendence/settings.py
+++ srcs/requeriments/gunicorn/proyect/trascendence/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-yrb=73%bos5rsni@-qb0c&2)uwi#n4g^yyhhzr1=)!yf13ws5+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '0.0.0.0',
+]
 
 
 # Application definition
