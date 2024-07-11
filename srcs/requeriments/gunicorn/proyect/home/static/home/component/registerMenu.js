ee36b3d7a5bf97cc2c6df2483061d17058b7c1dc
Pablo alberto Hijano rueda <phijano42@gmail.com>
2024-07-11 12:31:49 +0200

    added menu for sign up and login

---
+++ srcs/requeriments/gunicorn/proyect/home/static/home/component/registerMenu.js
@@ -0,0 +1,9 @@
+export function registerMenu(){
+	const registerMenu = document.createElement("nav");
+
+	registerMenu.innerHTML = `
+	<a class="px-3 text-white text-decoration-none" href="/">Sign up<a>
+	<a class="px-3 text-white text-decoration-none"href="/test">Login<a>
+	`;
+	return registerMenu;
+}
