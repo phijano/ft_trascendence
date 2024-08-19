9ba18eb4ec8b49139ce5a560d8d82968240d36f5
Pablo alberto Hijano rueda <phijano42@gmail.com>
2024-08-19 12:52:09 +0200

    fixed consumer

--- srcs/requeriments/gunicorn/proyect/userManagement/consumers.py
+++ srcs/requeriments/gunicorn/proyect/userManagement/consumers.py
@@ -2,16 +2,24 @@ from channels.generic.websocket import WebsocketConsumer
 from .models import Profile
 
 class WebConsumer(WebsocketConsumer):
+
     def connect(self):
-        self.accept()
-        user_profile = Profile.objects.get(user_id=self.scope['user'])
-        user_profile.connections = user_profile.connections + 1
-        user_profile.save()
-        print("connected: ")
+        if self.scope['user'].is_authenticated:
+            user_profile = Profile.objects.get(user_id=self.scope['user'])
+            user_profile.connections = user_profile.connections + 1
+            user_profile.save()
+            print("connected")
+            self.accept()
+        else:
+            print("rejected")
+            self.close()
+
     def disconnect(self, close_code):
-        user_profile = Profile.objects.get(user_id=self.scope['user'])
-        user_profile.connections = user_profile.connections - 1
-        user_profile.save()
-        print("disconnected")
+        if not self.scope['user'].is_anonymous:
+            user_profile = Profile.objects.get(user_id=self.scope['user'])
+            user_profile.connections = user_profile.connections - 1
+            user_profile.save()
+            print("disconnected")
+
     def receive(self, text_data):
         print("comunicating")
