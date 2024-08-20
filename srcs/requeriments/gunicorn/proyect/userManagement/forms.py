f42b9021b6be1853fe278bcd386de54922997eb5
Pablo alberto Hijano rueda <phijano42@gmail.com>
2024-08-20 12:47:06 +0200

    removed unnecesary form

--- srcs/requeriments/gunicorn/proyect/userManagement/forms.py
+++ srcs/requeriments/gunicorn/proyect/userManagement/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from .models import Friendship
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -9,10 +8,3 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-
-class FriendshipCreationForm(forms.ModelForm):
-    class Meta:
-        model = Friendship
-        fields = ()
-        
-
