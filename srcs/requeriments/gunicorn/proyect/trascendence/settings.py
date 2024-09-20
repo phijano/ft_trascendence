2203ecf474e6954704325d3133b798d824ebd993
rdelicad <rdelicad@gmail.com>
2024-09-20 10:33:41 +0200

    agregada pagina de chat funcionando

--- srcs/requeriments/gunicorn/proyect/trascendence/settings.py
+++ srcs/requeriments/gunicorn/proyect/trascendence/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'my_secret_key'
+SECRET_KEY = 'django-insecure-yrb=73%bos5rsni@-qb0cdjango-insecure-yrb=73%bos5rsni@-qb0cdjango-insecure-yrb=73%bos5rsni@-qb0cdjango-insecure-yrb=73%bos5rsni@-qb0cdjango-insecure-yrb=73%bos5rsni@-qb0cmy_secret_key2)uwi#n4g^yyhhzr1=)!yf13ws5+2)uwi#n4g^yyhhzr1=)!yf13ws5+2)uwi#n4g^yyhhzr1=)!yf13ws5+2)uwi#n4g^yyhhzr1=)!yf13ws5+2)uwi#n4g^yyhhzr1=)!yf13ws5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,7 +54,9 @@ INSTALLED_APPS = [
     'userManagement',
     'pong',
     'chat',
-    'channels',	
+    'channels',
+    'django_htmx',
+    'django_cleanup',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_db_name',
-        'USER': 'my_db_user',
-        'PASSWORD': 'my_db_password',
+        'NAME': 'trascendence',
+        'USER': 'dbmaster',
+        'PASSWORD': 'masterpass',
         'HOST': 'postgre',
         'PORT': '5432',
     }
@@ -164,8 +166,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 #EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'my_email_host_user_onemy_email_host_user_two'
-EMAIL_HOST_PASSWORD = 'my_email_host_password'
+EMAIL_HOST_USER = 'mvptrascendence42@gmail.com'
+EMAIL_HOST_PASSWORD = 'qskq yany alqe suwe'
 EMAIL_PORT = 587
 
 # Static files (CSS, JavaScript, Images)
