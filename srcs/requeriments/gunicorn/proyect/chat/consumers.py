80ff5b89162a3aec12fd7d28dccbd2cc84f18eff
rdelicad <rdelicad@gmail.com>
2024-09-23 18:16:46 +0200

    cliente-servidor chateando 1 a 1

--- srcs/requeriments/gunicorn/proyect/chat/consumers.py
+++ srcs/requeriments/gunicorn/proyect/chat/consumers.py
@@ -1,18 +1,28 @@
 from channels.generic.websocket import WebsocketConsumer
+from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
+import json
+from .models import *
 
 class ChatroomConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
+        self.chatroom_name = self.scope['url_route']['kwargs']['chatroom_name']
+        self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
         self.accept()
 
-    """ def disconnect(self, close_code):
-        pass
-
     def receive(self, text_data):
-        # Puedes parsear y manejar datos JSON entrantes
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        body = text_data_json['body']
 
-        # Enviar mensaje de vuelta al cliente
-        self.send(text_data=json.dumps({
-            'message': message
-        })) """
+        message = GroupMessage.objects.create(
+            body = body,
+            author = self.user,
+            group = self.chatroom
+        )
+        context = {
+            'message': message,
+            'user': self.user
+        }
+        html = render_to_string('chat/partials/chat_message_p.html', context=context)
+        self.send(text_data=html)
