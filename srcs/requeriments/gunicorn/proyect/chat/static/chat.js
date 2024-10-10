1b766d068ee6fe4d137d0a01ef85e79cdce6599c
rdelicad <rdelicad@gmail.com>
2024-10-10 22:06:33 +0200

    estilos chat bien, falta websocket

---
+++ srcs/requeriments/gunicorn/proyect/chat/static/chat.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const chatroomName = 'public-chat'; // Cambia esto si necesitas un nombre de sala dinámico
+    const chatSocket = new WebSocket(
+        'ws://' + window.location.host + '/ws/chatroom/' + chatroomName + '/'
+    );
+
+    // Manejar mensajes recibidos del WebSocket
+    chatSocket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+        const messageList = document.getElementById('chat_messages');
+        const messageItem = document.createElement('li');
+        messageItem.innerHTML = `
+            <div class="flex justify-start">
+                <div class="flex items-end mr-2">
+                    <a href="/profile/${data.author}">
+                        <img class="w-8 h-8 rounded-full object-cover" src="${data.avatar}">
+                    </a>
+                </div>
+                <div class="bg-white p-4 max-w-[75%] rounded-r-lg rounded-tl-lg">
+                    <span>${data.body}</span>
+                </div>
+            </div>
+        `;
+        messageList.appendChild(messageItem);
+        scrollToBottom();
+    };
+
+    // Manejar cierre del WebSocket
+    chatSocket.onclose = function(e) {
+        console.error('Chat socket closed unexpectedly');
+    };
+
+    // Manejar envío de mensajes
+    document.getElementById('chat_message_form').onsubmit = function(e) {
+        e.preventDefault();
+        const messageInputDom = document.getElementById('message_input');
+        const message = messageInputDom.value;
+
+        // Enviar mensaje al servidor Django
+        fetch('/chat/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+            },
+            body: JSON.stringify({
+                'message': message
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Enviar mensaje al WebSocket
+            chatSocket.send(JSON.stringify({
+                'message': data.body,
+                'author': data.author,
+                'avatar': data.avatar,
+                'is_author': data.is_author
+            }));
+            messageInputDom.value = '';
+        });
+    };
+
+    // Enviar mensaje al presionar "Enter"
+    document.getElementById('message_input').addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            document.getElementById('chat_message_form').submit();
+        }
+    });
+});
