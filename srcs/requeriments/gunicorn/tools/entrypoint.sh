d422306f242b42b232380327032b991720cc909f
rdelicad <rdelicad@gmail.com>
2025-01-16 10:19:47 +0100

    contenedores iniciados con make

--- srcs/requeriments/gunicorn/tools/entrypoint.sh
+++ srcs/requeriments/gunicorn/tools/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo "Esperando a que PostgreSQL esté listo..."
+echo "Waiting for PostgreSQL to be ready..."
 until pg_isready -h postgre -U "$DB_USER" -d "$DB_NAME"; do
   sleep 1
 done
 
-echo "PostgreSQL está listo. Ejecutando migraciones..."
+echo "PostgreSQL is ready. Running migrations..."
 python /trascendence/manage.py makemigrations
 python /trascendence/manage.py migrate
 python /trascendence/manage.py create_users
 
-echo "Iniciando Gunicorn..."
+echo "Starting Gunicorn..."
 #exec trascendence/manage.py runserver 0.0.0.0:8081
 exec gunicorn -b 0.0.0.0:8081 --chdir /trascendence trascendence.asgi:application -k uvicorn.workers.UvicornWorker
 #exec "$@"
